7037c7d8fccdacc01cbcb593ccb506426c1d14f9
Ive <ive_tourinho@hotmail.com>
2024-05-06 21:53:44 -0300

    styles: modal size

--- src/components/Modal/styled.ts
+++ src/components/Modal/styled.ts
@@ -7,9 +7,19 @@ export const StyledModal = styled.div`
   padding: 16px;
   place-self: center center;
   border-radius: 4px;
-  min-width: 50%;
+  min-width: 40%;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   z-Index: 10;
+
+  @media only screen and (min-width: 500px) {
+    max-width: 80%;
+    width: 100%;
+  }
+
+  @media only screen and (min-width: 768px) {
+    max-width: 50%;
+    width: 100%;
+  }
 
   >:first-child {
     display: flex;
